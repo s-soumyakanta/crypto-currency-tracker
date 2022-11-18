72ea99c74df6e85986453aa6a1808adcab16b341
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2022-11-19 04:54:22 +0530

    Major change ! used an array to store api data.

--- src/components/CoinTable.js
+++ src/components/CoinTable.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 export default function CoinTable() {
-    const [apiData, setApiData] = useState({})
+    const [apiData, setApiData] = useState([])
 
     useEffect(() => {
         axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h")
@@ -11,18 +11,11 @@ export default function CoinTable() {
     }, []
 
     )
-    console.log(apiData)
-
     return (
-
         <div className="container">
             <div className="serch">Serch</div>
             <div className="table">
-
-
-
                 <table>
-
                     <tr>
                         <th>Rank</th>
                         <th>Coin</th>
@@ -37,33 +30,24 @@ export default function CoinTable() {
                     {
                         apiData.map((data) => {
                             return (
-
-
-
                                 <tr>
                                     <td>{data.market_cap_rank}</td>
                                     <td>{data.name}</td>
                                     <td>&#36;{data.current_price
                                     }</td>
-                                    <td>{data.price_change_percentage_24h}</td>
+                                    <td>{data.price_change_percentage_24h.toPrecision(2)}&#37;</td>
+
                                     <td>{data.market_cap
                                     }</td>
+
                                     <td>{data.total_supply}</td>
                                     <td>{data.total_supply}</td>
                                 </tr>
-
                             )
                         })
                     }
                 </table>
-
-
-
-
             </div>
-
-
-
         </div>
     )
 }
